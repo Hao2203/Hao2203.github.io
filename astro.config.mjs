12802dd2f3216e99b33b261b6911be12cd48e807
Hao2203 <516501366@qq.com>
2023-11-10 22:53:36 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Hao2203.github.io',
   integrations: [tailwind(), solidJs()],
-  // output: "server",
-  // adapter: node({
-  //   mode: "standalone"
-  // })
 });
